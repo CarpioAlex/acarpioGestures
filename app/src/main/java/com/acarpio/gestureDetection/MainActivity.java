6eceeed178db90877c063440b278b6ea0727408a
Alex <acarpiomoratalla@cifpfbmoll.eu>
2024-10-16 14:07:03 +0200

    Added exercise

---
+++ app/src/main/java/com/acarpio/gestureDetection/MainActivity.java
@@ -0,0 +1,85 @@
+package com.acarpio.gestureDetection;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Gesture detector
+    private GestureDetectorCompat mDetector1;
+    private GestureDetectorCompat mDetector2;
+    // Debug constants
+    private static final String DEBUG_TAG = "Gestures";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        // Edges
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize the gesture detector
+        // (this (context), new GestureDetector, app context for the toast)
+        mDetector1 = new GestureDetectorCompat(this, new FlingDetector(this, findViewById(R.id.pelota)));
+        mDetector2 = new GestureDetectorCompat(this, new SmallRectangleDetector(this));
+
+        // Getting the view of my "sweapable area"
+
+        View areaGestos = findViewById(R.id.swipableFrame);
+        View areaTouch = findViewById(R.id.touchableArea);
+
+
+
+        // Big rectangle listener
+        areaGestos.setOnTouchListener(new View.OnTouchListener(){
+
+
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.getId() == R.id.swipableFrame) {
+                    mDetector1.onTouchEvent(event);
+                    return true;
+                }
+                return false;
+                }
+
+        });
+
+        // Small rectangle listener
+
+        areaTouch.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.getId() == R.id.touchableArea) {
+                    mDetector2.onTouchEvent(event);
+                    return true;
+                }
+                return false;
+                }
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+}
